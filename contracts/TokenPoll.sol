20d1c53d90b3873d4350b639a04ef5a23e6c3f79
Nick Geoca <nickgeoca@gmail.com>
2018-07-29 14:03:05 -0500

    Add autostart round example

--- contracts/TokenPoll.sol
+++ contracts/TokenPoll.sol
@@ -344,3 +344,30 @@ contract TokenPoll is Ownable {
   }
 
  */
+
+
+/*
+    function delayedPayment(
+        address _scheduler,
+        uint    _numBlocks,
+        address _recipient
+    )  public payable {
+        scheduler = SchedulerInterface(_scheduler);
+        lockedUntil = block.number + _numBlocks;
+        recipient = _recipient;
+
+        scheduledTransaction = scheduler.schedule.value(0.1 ETHER)( // 0.1 ether is to pay for gas, bounty and fee
+            this,                   // send to self
+            "",                     // and trigger fallback function
+            [ 200000             // The amount of gas to be sent with the transaction.
+            , 0                  // The amount of wei to be sent.
+            , 255                // The size of the execution window.
+            , lockedUntil        // The start of the execution window.
+            , 20000000000 wei    // The gasprice for the transaction (aka 30 gwei)
+            , 20000000000 wei    // The fee included in the transaction.
+            , 20000000000 wei    // The bounty that awards the executor of the transaction.
+            , 30000000000 wei    // The required amount of wei the claimer must send as deposit.
+            ]
+        );
+    }
+*/
