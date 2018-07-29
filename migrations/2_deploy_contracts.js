0fc60ab94ec9b98ec81183db651a58e8124528d8
Nick Geoca <nickgeoca@gmail.com>
2018-07-29 14:36:16 -0500

    update migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var SafeMath = artifacts.require('SafeMath.sol');
-var TokenPollFactory = artifacts.require('TokenPollFactory.sol');
+var SafeMath = artifacts.require('./lib/SafeMath.sol');
+var TokenPollFactory = artifacts.require('./tokenPoll/TokenPollFactory.sol');
+var MultiSigWalletFactory = artifacts.require('./stash/MultiSigWalletFactory.sol');
+var CreateStash = artifacts.require('./CreateStash.sol');
 
 module.exports = function(deployer, network, accounts) {
   return deployer.deploy(SafeMath).then(async () => {
@@ -7,6 +9,9 @@ module.exports = function(deployer, network, accounts) {
     await deployer.link(SafeMath, TokenPollFactory);
 
     await deployer.deploy(TokenPollFactory);
-    
+    await deployer.deploy(MultiSigWalletFactory);
+
+    await deployer.deploy(CreateStash, TokenPollFactory.address, MultiSigWalletFactory.address, '0x0', '0');
   });
 }
+
