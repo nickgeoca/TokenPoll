26ff3d5eb4451d4cb47db4b94c6afab82cb652cd
Nick Geoca <nickgeoca@gmail.com>
2018-10-31 17:59:40 -0500

    Constrain to one stable coin

--- contracts/TokenPollFactory.sol
+++ contracts/TokenPollFactory.sol
@@ -7,10 +7,14 @@ import "./Ownable.sol";
 contract TokenPollFactory {
   event TokenPollCreated(address indexed sender, address tokenPoll);
 
-  function TokenPollFactory () {}
+  address public stableCoin;
+
+  function TokenPollFactory (address _stableCoin) {
+    stableCoin = _stableCoin;
+  }
 
   function createTokenPoll() {
-    TokenPoll tp = new TokenPoll();
+    TokenPoll tp = new TokenPoll(stableCoin);
     tp.transferOwnership(msg.sender);
     TokenPollCreated(msg.sender, tp);
   }
