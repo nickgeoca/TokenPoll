a1ccd7987d6ff05341ca3d079c41370e52e20b54
Nick Geoca <nickgeoca@gmail.com>
2019-01-21 09:06:55 -0800

    bug fix- tooling change

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ var TokenPollFactory = artifacts.require('TokenPollFactory.sol');
 
 module.exports = function(deployer, network, accounts) {
   return deployer.deploy(SafeMath).then(async () => {
-    if (network != 'rinkeby' && network != 'development') throw('Update stable coin address!');
+    if (network != 'rinkeby-fork' && network != 'development') throw('Update stable coin address!');
     await deployer.link(SafeMath, TokenPollFactory);
     await deployer.deploy(TokenPollFactory, '0x9e621781C6D70c50551F6757CF320BDE0191d52E');
   });
